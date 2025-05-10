a6e4d73809021947f4b545bc4fddeeb0d811fb5a
Rise <64920400+frizerrer@users.noreply.github.com>
2025-05-10 17:06:56 +0300

    First

---
+++ Computer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ComputerApp
+{
+    public class Computer
+    {
+        
+        private int inventoryNumber;
+        private double processorFrequencyGHz;
+        private int hardDriveSizeGB;
+
+
+        public int InventoryNumber
+        {
+            get { return inventoryNumber; }
+            set { inventoryNumber = value; }
+        }
+
+        public double ProcessorFrequencyGHz
+        {
+            get { return processorFrequencyGHz; }
+            set { processorFrequencyGHz = value; }
+        }
+
+        public int HardDriveSizeGB
+        {
+            get { return hardDriveSizeGB; }
+            set { hardDriveSizeGB = value; }
+        }
+
+        
+        public Computer(int number, double frequency, int memory)
+        {
+            inventoryNumber = number;
+            processorFrequencyGHz = frequency;
+            hardDriveSizeGB = memory;
+        }
+
+        
+        public void ShowInfo()
+        {
+            Console.WriteLine("Инвентарный номер: " + inventoryNumber);
+            Console.WriteLine("Частота процессора: " + processorFrequencyGHz + " ГГц");
+            Console.WriteLine("Объем жёсткого диска: " + hardDriveSizeGB + " ГБ");
+        }
+
+        
+        public double CalculateMemoryCost(double costPerGB)
+        {
+            return hardDriveSizeGB * costPerGB;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ComputerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Computer comp = new Computer(123345, 1.5, 400);
+
+            
+            comp.ShowInfo();
+
+           
+            double pricePerGB = 100;
+            double totalCost = comp.CalculateMemoryCost(pricePerGB);
+            Console.WriteLine("Стоимость внешней памяти: " + totalCost + " рублей");
+
+            Console.ReadLine();
+        }
+    }
+}
